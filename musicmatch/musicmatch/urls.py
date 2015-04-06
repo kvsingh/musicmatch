ee1d5323db79531e2330727cacf0bab96e66ed7a
Karan <karan.kornguy@gmail.com>
2015-04-07 01:53:03 +0530

    added django project

---
+++ musicmatch/musicmatch/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'musicmatch.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', include('match.urls')),                       
+    url(r'^admin/', include(admin.site.urls)),
+)
